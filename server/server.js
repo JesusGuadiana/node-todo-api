41f032c1a63dadbb3bcfb4cdfc13de298f86f2ea
JesusGuadiana <jisus130@hotmail.com>
2018-08-07 10:36:33 -0500

    Created tests for DELETE/todos/id

--- server/server.js
+++ server/server.js
@@ -62,7 +62,7 @@ app.post("/todos", (req, res) => {
       if(!todo){
         return res.status(404).send();
       }
-      res.status(200).send(todo);
+      res.status(200).send({todo});
     }).catch((e) => res.status(400).send());
   });
 
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -102,3 +102,42 @@ describe("GET /todos/:id", () => {
       .end(done)
   });
 });
+
+describe("DELETE /todos/:id", () => {
+  it("Should delete a todo", (done) => {
+     var hexId = todos[1]._id.toHexString();
+
+    request(app)
+      .delete("/todos/" + hexId)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err, res) =>{
+        if(err){
+          return done(err);
+        }
+        //query database using ding byID
+        Todo.findById(hexId).then((todo) => {
+          expect(todo).toNotExist();
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it("Should return a 404 if todo not found", (done) => {
+    var randomId = new ObjectID;
+
+    request(app)
+      .delete("/todos/" + randomId.toHexString())
+      .expect(404)
+      .end(done)
+  });
+
+  it("Should return 404 for non object IDS", (done) => {
+    request(app)
+      .delete("/todos/123")
+      .expect(404)
+      .end(done)
+  });
+});
